360effd73e00818099faa816b0436e1a119e513a
Sacha Chua <sacha@sachachua.com>
2021-01-03 02:08:04 -0500

    Add HTML

--- emacs-calendar.sh
+++ emacs-calendar.sh
@@ -1,7 +1,13 @@
+timezones='Etc/GMT
+America/Toronto
+America/Vancouver
+Asia/Kolkata
+Asia/Singapore
+Europe/Berlin'
 wget -q -N https://calendar.google.com/calendar/ical/c_rkq3fc6u8k1nem23qegqc90l6c%40group.calendar.google.com/public/basic.ics -O emacs-calendar.ics
-./ics-to-org --sort -f Etc/GMT emacs-calendar.ics > emacs-calendar-gmt.org
-./ics-to-org --sort -f America/Toronto emacs-calendar.ics > emacs-calendar-toronto.org
-./ics-to-org --sort -f America/Vancouver emacs-calendar.ics > emacs-calendar-vancouver.org
-./ics-to-org --sort -f Asia/Kolkata emacs-calendar.ics > emacs-calendar-kolkata.org
-./ics-to-org --sort -f Asia/Singapore emacs-calendar.ics > emacs-calendar-singapore.org
-./ics-to-org --sort -f Europe/Berlin emacs-calendar.ics > emacs-calendar-berlin.org
+DATE=$(date +"%Y%m01")
+for ZONE in $timezones; do
+  STUB=$(echo $ZONE | sed s/^.*\\/// | tr A-Z a-z)
+  ./ics-to-org --sort -f $ZONE emacs-calendar.ics > emacs-calendar-$STUB.org
+  ical2html -l -f "Times are in $ZONE" -z $ZONE $DATE P8W emacs-calendar.ics | perl -e 'undef $/; s/<span class=summary>([^<]+)<\/span>\n<pre><b class=location>([^<]+)<\/b><\/pre>/<span class="summary"><a href="$2">$1<\/a><\/span>/' > emacs-calendar-$STUB.html
+done
