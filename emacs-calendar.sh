1bb78d7dad284963885242fac94dc92c2f056925
Sacha Chua <sacha@sachachua.com>
2021-01-25 00:53:40 -0500

    Add new events

--- emacs-calendar.sh
+++ emacs-calendar.sh
@@ -8,6 +8,6 @@ wget -q -N https://calendar.google.com/calendar/ical/c_rkq3fc6u8k1nem23qegqc90l6
 DATE=$(date +"%Y%m01")
 for ZONE in $timezones; do
   STUB=$(echo $ZONE | sed s/^.*\\/// | tr A-Z a-z)
-  ./ics-to-org --sort -f $ZONE emacs-calendar.ics > emacs-calendar-$STUB.org
+  ruby ./ics-to-org --sort -f $ZONE emacs-calendar.ics > emacs-calendar-$STUB.org
   ical2html -l -f "Times are in $ZONE" -z $ZONE $DATE P8W emacs-calendar.ics | perl -e 'undef $/; s/<span class=summary>([^<]+)<\/span>\n<pre><b class=location>([^<]+)<\/b><\/pre>/<span class="summary"><a href="$2">$1<\/a><\/span>/' > emacs-calendar-$STUB.html
 done
