82be8a73316a79acf8b27541b98e73a1ccfec17f
Sacha Chua <sacha@sachachua.com>
2021-04-05 09:55:12 -0400

    Update

--- emacs-calendar.sh
+++ emacs-calendar.sh
@@ -1,14 +1,12 @@
-timezones='Etc/GMT
+timezones='America/Vancouver
+America/Chicago
 America/Toronto
-America/Vancouver
+Etc/GMT
+Europe/Berlin
 Asia/Kolkata
-Asia/Shanghai
-Asia/Singapore
-Europe/Berlin'
-  wget -q -N https://calendar.google.com/calendar/ical/c_rkq3fc6u8k1nem23qegqc90l6c%40group.calendar.google.com/public/basic.ics -O emacs-calendar.ics
+Asia/Singapore'
   DATE=$(date +"%Y%m01")
   for ZONE in $timezones; do
     STUB=$(echo $ZONE | sed s/^.*\\/// | tr A-Z a-z)
-    ./ics-to-org --sort -f $ZONE emacs-calendar.ics > emacs-calendar-$STUB.org
     ical2html -l -f "Times are in $ZONE" -z $ZONE $DATE P8W emacs-calendar.ics | perl -e 'undef $/; $_ = <>; $_ =~ s/<span class=summary>([^<]+)<\/span>\n<pre><b class=location>([^<]+)<\/b><\/pre>/<span class="summary"><a href="$2">$1<\/a><\/span>/g; print $_' > emacs-calendar-$STUB.html
   done
